cc823932c73b43062af6522f0641031549e8b124
fatima201900571 <fatimaalzahraa.maarouf@lau.edu>
2021-11-23 23:47:33 +0200

    first commit3

--- Add Account/colorlib-regform-5/index.php
+++ Add Account/colorlib-regform-5/index.php
@@ -6,7 +6,7 @@ include("../../connection.php");
 // $stmt = $connection->prepare($query);
 // $stmt->execute();
 // $results = $stmt->get_result();
-echo "connected";
+echo "connected here";
 
 ?>
 <!DOCTYPE html>
@@ -36,6 +36,7 @@ echo "connected";
       rel="stylesheet"
       media="all"
     />
+
     <!-- Font special for pages-->
     <link
       href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
